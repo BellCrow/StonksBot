56449b4be010ac7665cd9ff5f287b457ca951a98
josh <peremptor1@gmx.de>
2022-05-22 12:17:59 +0200

    Add sample discord integration project and code for connecting the bot to a discord server; change gitignore to not include token files with tokens for discord bot api

---
+++ source/StonksBot.Integration.Discord/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Discord;
+using Discord.WebSocket;
+
+public class MainClass
+{
+    //tutorial for this code is here https://discordnet.dev/guides/getting_started/first-bot.html
+    public static async Task Main(string[] args)
+    {
+        var discordApiToken = LoadToken("DiscordApi.token");
+        var client = new DiscordSocketClient();
+        client.Log += Log;
+
+        await client.LoginAsync(TokenType.Bot, discordApiToken);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage message)
+    {
+        Console.WriteLine(message.ToString());
+        return Task.CompletedTask;
+    }
+
+    private static string LoadToken(string tokenFileName)
+    {
+        return File.ReadAllText(tokenFileName);
+    }
+}
+
